4081c4f2af4ad9608e927fe42ae2515c05dbe226
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2019-04-13 16:00:10 +0200

    Add extra information in example

--- examples/serverless-node6.10.3-custom-runtime/handler.js
+++ examples/serverless-node6.10.3-custom-runtime/handler.js
@@ -4,8 +4,20 @@ module.exports.hello = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
+      message: 'Your function executed successfully using a custom runtime!',
       version: process.version,
+      _HANDLER: process.env._HANDLER,
+      AWS_REGION: process.env.AWS_REGION,
+      AWS_EXECUTION_ENV: process.env.AWS_EXECUTION_ENV,
+      AWS_LAMBDA_FUNCTION_NAME: process.env.AWS_LAMBDA_FUNCTION_NAME,
+      AWS_LAMBDA_FUNCTION_MEMORY_SIZE: process.env.AWS_LAMBDA_FUNCTION_MEMORY_SIZE,
+      AWS_LAMBDA_FUNCTION_VERSION: process.env.AWS_LAMBDA_FUNCTION_VERSION,
+      TZ: process.env.TZ,
+      LAMBDA_TASK_ROOT: process.env.LAMBDA_TASK_ROOT,
+      LAMBDA_RUNTIME_DIR: process.env.LAMBDA_RUNTIME_DIR,
+      PATH: process.env.PATH,
+      LD_LIBRARY_PATH: process.env.LD_LIBRARY_PATH,
+      AWS_LAMBDA_RUNTIME_API: process.env.AWS_LAMBDA_RUNTIME_API,
     }),
   };
 
